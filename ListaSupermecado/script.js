1fb6e6ca4123e5533a67f0e66e22d39c8f1fd369
zZanderZz <110944106+zZanderZz@users.noreply.github.com>
2023-03-15 22:21:48 -0300

    Add files via upload

--- ListaSupermecado/script.js
+++ ListaSupermecado/script.js
@@ -1,11 +1,33 @@
 const input = window.document.getElementById("input");
 const ul = window.document.querySelector("ul")
 
-function adicionar(){
+function adicionar() {
     var li = window.document.createElement("li");
     var value = input.value;
     var value_li = window.document.createTextNode(value);
 
-    li.appendChild(value_li);
-    ul.appendChild(li);
+    if (input.value.trim() !== "") {
+        li.appendChild(value_li);
+        ul.appendChild(li);
+    }
+
+     input.value = "";
+     input.focus();
 }
+
+window.document.getElementById("ul").addEventListener("click", (e) => {
+    e.target.classList.toggle("checked");
+});
+
+window.document.getElementById("input").addEventListener("keypress", (e) =>{
+    if(e.key === "Enter"){
+        adicionar();
+    }
+});
+
+window.document.addEventListener("contextmenu", (e) =>{
+    e.preventDefault();
+    if(e.target.tagName === "LI"){
+        e.target.remove();
+    }
+});
