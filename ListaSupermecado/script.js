5209eed6435aab01d3afce9e5bd8c17ba618ae4d
zZanderZz <110944106+zZanderZz@users.noreply.github.com>
2023-03-16 22:24:48 -0300

    Add files via upload

--- ListaSupermecado/script.js
+++ ListaSupermecado/script.js
@@ -1,19 +1,29 @@
 const input = window.document.getElementById("input");
-const ul = window.document.querySelector("ul")
+const ul = window.document.querySelector("ul");
+const db_lista_li = JSON.parse(window.localStorage.getItem("Lista"));
 
-function adicionar() {
+db_lista_li.forEach((item) => adicionar(item));
+
+function adicionar(item) {
     var li = window.document.createElement("li");
-    var value = input.value;
+    var value = item ? item.texto: input.value;
     var value_li = window.document.createTextNode(value);
+    }
+
 
     if (input.value.trim() !== "") {
         li.appendChild(value_li);
         ul.appendChild(li);
     }
 
+    if(!item){
+        adiciona_lista();
+    }
+
+    adiciona_lista();
+
      input.value = "";
      input.focus();
-}
 
 window.document.getElementById("ul").addEventListener("click", (e) => {
     e.target.classList.toggle("checked");
@@ -31,3 +41,15 @@ window.document.addEventListener("contextmenu", (e) =>{
         e.target.remove();
     }
 });
+
+function adiciona_lista(){
+    const lista_de_li = window.document.querySelectorAll("li");
+    const lista = [];
+    lista_de_li.forEach((item) => {
+        lista.push({texto : item.innerText});
+    });
+
+
+    window.localStorage.setItem("Lista",JSON.stringify(lista));
+
+}
